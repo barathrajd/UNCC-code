97429a9556968296ed38cda96d367188945eeecd
Barathraj <barathdeva757@gmail.com>
2023-12-11 08:45:33 +0530

    buffer data using Buffer.from

--- buffers/challange.js
+++ buffers/challange.js
@@ -9,8 +9,17 @@ memoryContainer[0] = 0x48;
 memoryContainer[1] = 0x69;
 memoryContainer[2] = 0x21;
 
-console.log(memoryContainer);
-console.log(memoryContainer[0]);
-console.log(memoryContainer[1]);
-console.log(memoryContainer[2]);
-console.log(memoryContainer.toString('utf-8'));
+// console.log(memoryContainer);
+// console.log(memoryContainer[0]);
+// console.log(memoryContainer[1]);
+// console.log(memoryContainer[2]);
+// console.log(memoryContainer.toString('utf-8'));
+
+const buff = Buffer.from([0x48, 0x69, 0x21]);
+console.log(buff.toString('utf-8'));
+
+const buff2 = Buffer.from('486921', 'hex');
+console.log(buff2.toString('utf-8'));
+
+const buff3 = Buffer.from('Hi!', 'utf-8');
+console.log(buff3);
