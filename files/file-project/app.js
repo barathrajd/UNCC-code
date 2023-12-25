b5b836517a37cbc5a4a1a8232616ac7f0b83fd7b
Barathraj <barathdeva757@gmail.com>
2023-12-25 14:01:37 +0530

    create a file

--- files/file-project/app.js
+++ files/file-project/app.js
@@ -1,6 +1,24 @@
 const fs = require('fs/promises');
 
+const createFile = async (filePath) => {
+  // await fs.writeFile(filePath, '');
+  try {
+    const exisitingFileHandle = await fs.open(filePath, 'r');
+    exisitingFileHandle.close();
+    return console.log(`This file ${filePath} already exists.`);
+  } catch (error) {
+    // we don't have the file now we
+    // console.log(error);
+    const newFileHandle = await fs.open(filePath, 'w');
+    console.log('A new File created successfully.');
+    newFileHandle.close();
+  }
+};
+
 (async () => {
+  // commands
+  const CREATE_FILE = 'create a file';
+
   const commandFileHandler = await fs.open('./command.txt', 'r');
 
   commandFileHandler.on('change', async () => {
@@ -16,8 +34,15 @@ const fs = require('fs/promises');
     const length = buffer.byteLength;
     await commandFileHandler.read(buffer, offset, length, position);
 
-    console.log(buffer.toString('utf-8'));
     // Need to read the conent
+    const command = buffer.toString('utf-8');
+
+    // create a file
+    // create a file <path>
+    if (command.includes(CREATE_FILE)) {
+      const filePath = command.substring(CREATE_FILE.length + 1);
+      createFile(filePath);
+    }
   });
 
   const watcher = fs.watch('./');
