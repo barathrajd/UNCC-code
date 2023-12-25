e96a153c348abde5c6f56b2f591467e2b3f752da
Barathraj <barathdeva757@gmail.com>
2023-12-25 13:26:54 +0530

    fs.open -> filehandler

--- files/file-project/app.js
+++ files/file-project/app.js
@@ -1,14 +1,28 @@
 const fs = require('fs/promises');
 
 (async () => {
+  const commandFileHandler = await fs.open('./command.txt', 'r');
+
   const watcher = fs.watch('./');
 
   for await (const event of watcher) {
     if (event.eventType === 'change') {
       //the file changed
-      console.log('The file changes');
-      const data = await fs.readFile('./command.txt');
-      console.log(data.toString('ascii'));
+      console.log('The file was changed');
+
+      // Get the size of the file
+      const size = (await commandFileHandler.stat()).size;
+      const buffer = Buffer.alloc(size);
+      const offset = 0;
+      const position = 0;
+      const length = size;
+      const content = await commandFileHandler.read(
+        buffer,
+        offset,
+        length,
+        position
+      );
+      console.log(content);
     }
   }
 })();
