804d6f3e63d01251862b5febaaf0c6e6dd6c24a6
Barathraj <barathdeva757@gmail.com>
2023-12-25 13:32:57 +0530

    refactor

--- files/file-project/app.js
+++ files/file-project/app.js
@@ -3,26 +3,31 @@ const fs = require('fs/promises');
 (async () => {
   const commandFileHandler = await fs.open('./command.txt', 'r');
 
+  commandFileHandler.on('change', async () => {
+    // Get the size of the file
+    const size = (await commandFileHandler.stat()).size;
+    // Allocate the buffer size
+    const buffer = Buffer.alloc(size);
+    // the location at which we want to start the filling our buffer
+    const offset = 0;
+    // the location at which we want to start the reading
+    const position = 0;
+    // length of the file
+    const length = buffer.byteLength;
+    const content = await commandFileHandler.read(
+      buffer,
+      offset,
+      length,
+      position
+    );
+    console.log(content);
+  });
+
   const watcher = fs.watch('./');
 
   for await (const event of watcher) {
     if (event.eventType === 'change') {
-      //the file changed
-      console.log('The file was changed');
-
-      // Get the size of the file
-      const size = (await commandFileHandler.stat()).size;
-      const buffer = Buffer.alloc(size);
-      const offset = 0;
-      const position = 0;
-      const length = size;
-      const content = await commandFileHandler.read(
-        buffer,
-        offset,
-        length,
-        position
-      );
-      console.log(content);
+      commandFileHandler.emit('change');
     }
   }
 })();
