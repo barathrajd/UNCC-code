0c86fb13044b33e957229844cea349925f124306
Barathraj <barathdeva757@gmail.com>
2023-12-26 23:51:15 +0530

    Write file million times

--- streams/writeMany.js
+++ streams/writeMany.js
@@ -1,17 +1,30 @@
-const fs = require('node:fs/promises');
+// const fs = require('node:fs/promises');
 
-async function writeFile() {
-  try {
-    console.time('writeMany');
-    const file = await fs.open('text.txt', 'w');
+// async function writeFile() {
+//   try {
+//     console.time('writeMany');
+//     const file = await fs.open('text.txt', 'w');
+//     for (let index = 0; index < 1000000; index++) {
+//       file.write(` ${index} `);
+//     }
+//     file.close();
+//     console.timeEnd('writeMany');
+//   } catch (error) {
+//     console.log('Some error');
+//   }
+// }
+
+// writeFile();
+
+const fs = require('node:fs');
+
+(async () => {
+  console.time('writeMany');
+  fs.open('text.txt', 'w', (err, fd) => {
     for (let index = 0; index < 1000000; index++) {
-      file.write(` ${index} `);
-      file.close();
+      const buff = Buffer.from(` ${index} `, 'utf-8');
+      fs.writeSync(fd, buff);
     }
     console.timeEnd('writeMany');
-  } catch (error) {
-    console.log('Some error');
-  }
-}
-
-writeFile();
+  });
+})();
